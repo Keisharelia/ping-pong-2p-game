b0ea380ea5487da54dcbec69625d46d82ac5b4d3
Keisharelia <louisaarleryn@gmail.com>
2025-08-12 18:39:31 +0700

    Update ping pong 2p.py

--- ping pong 2p.py
+++ ping pong 2p.py
@@ -1,5 +1,6 @@
 from pygame import *
 
+# kelas utama dari bola & racket
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
@@ -12,6 +13,7 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+# menggerakkan racket 
 class Player(GameSprite):
     def update_left (self):
         keys = key.get_pressed()
@@ -36,17 +38,17 @@ display.set_caption('Ping pong R vs L')
 # background = transform.scale(image.load('background.jpg'), (700, 500))
 
 
-
 RIGHT = Player('racket.png', 640, 200, 4, 50, 90)
 LEFT = Player ('racket.png', 5, 200, 4, 50, 90)
 ball = GameSprite('bola.png', 300, 200, 0, 65, 65)
-speed_x = 5
-speed_y = 5
+speed_x = 3
+speed_y = 3
 
+# kalah/bola keluar frame
 font.init()
 font = font.Font(None, 70)
-win = font.render('YOU WINN !!', True, (255, 215, 0))
-lose = font.render('YOU LOSE :(', True, (180, 0, 0))
+right_lose = font.render('YOU LOSE p2 !!', True, (255, 215, 0))
+left_lose = font.render('YOU LOSE p1 :(', True, (180, 0, 0))
 
 #set scene background
 x1 = 100
@@ -61,32 +63,41 @@ game = True
 clock = time.Clock()
 while game:
 
+    # untuk mengaktifkan tombol close
     for e in event.get():
         if e.type == QUIT:
             game = False
 
+    # untuk game tetap berjalan
     if finish != True:
        window.fill((229, 204, 255))
        RIGHT.update_right()
        LEFT.update_left()
        ball.rect.x += speed_x
        ball.rect.y += speed_y
-      
+       
+        # pergerakan bola
+       if ball.rect.y > win_height - 50 or ball.rect.y < 20:
+           speed_y *= -1
+
+       if sprite.collide_rect(RIGHT, ball) or sprite.collide_rect(LEFT, ball):
+           speed_x *= -1
+           speed_y *= -1
+
+        # win or lose 
+       if ball.rect.x < -200:
+           finish = True
+           window.blit(left_lose, (200, 200))
+           gameover = True
+
+       if ball.rect.x > win_width:
+           finish = True
+           window.blit(right_lose, (200, 200))
+           gameover = True
+
        RIGHT.reset()
        LEFT.reset()
        ball.reset()
-
-
-    #    if sprite.collide_rect(packman, monster) or sprite.collide_rect(packman, w1)or sprite.collide_rect(packman, w2) or sprite.collide_rect(packman, w3) or sprite.collide_rect(packman, w4) or sprite.collide_rect(packman, w5) or sprite.collide_rect(packman, w6) or sprite.collide_rect(packman, w7):
-    #        window.blit(lose, (200, 200))  
-    #        finish = True
-    #        kick.play()
-
-    #    if sprite.collide_rect(packman, final):
-    #        window.blit(win, (200, 200))  
-    #        finish = True
-    #        money.play()
-
 
     display.update()
     clock.tick(60)
